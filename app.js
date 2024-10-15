a41d7f59d7691394822e50a1161dab62ca54ff9f
WaislanyaRilary <143360811+WaislanyaRilary@users.noreply.github.com>
2024-10-15 11:55:58 +0000

    alteracos

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ function criaCartao(categoria, pergunta, resposta) {
     
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3>$(categoria)</h3>
+    <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-       <p>$(pergunta)</p>
+       <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
     </div>
     </div>
     `
