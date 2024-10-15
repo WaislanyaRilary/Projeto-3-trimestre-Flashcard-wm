8eaafda36d9788ec50f7ade6e6cc40de6d523577
WaislanyaRilary <143360811+WaislanyaRilary@users.noreply.github.com>
2024-10-15 11:40:31 +0000

    alteracoes

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
     
-    cartao.innerHTML = 
+    cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>$(categoria)</h3>
     <div class="cartao__conteudo__pergunta">
        <p>$(pergunta)</p>
     </div>
     </div>
-    
-    
+    `
+
       let respostaEstaVisivel = false
       function viraCartao() {
         respostaEstaVisivel = !respostaEstaVisivel
